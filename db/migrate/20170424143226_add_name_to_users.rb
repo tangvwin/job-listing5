aa6183b7073490d5c2daca4cd979e430b769bd4a
Tony <tangvwin@gmail.com>
2017-05-01 12:55:35 +0800

    fail to change the user profile,ready to do it again

--- db/migrate/20170424143226_add_name_to_users.rb
+++ db/migrate/20170424143226_add_name_to_users.rb
@@ -1,7 +1,7 @@
 class AddNameToUsers < ActiveRecord::Migration[5.0]
   def change
      add_column :users, :name, :string
-     add_column :users, :data_of_birth, :datatime
+     add_column :users, :data_of_birth, :data
      add_column :users, :is_female, :boolean, default: false
 
   end
